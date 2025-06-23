213d267093e40e5e0ced2f4052851a3596d00330
Krutarth Kadia <202201475@daiict.ac.in>
2025-06-23 11:36:36 +0530

    Changing cors origin

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://adapt-taupe.vercel.app",
     credentials: true,
   })
 );
